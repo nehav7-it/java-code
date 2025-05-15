55510a699cccb3691998964fce43f240d32a1d81
Neha <nehav7.it@gmail.com>
2025-05-15 22:54:21 +0530

    Code for Adding int with float

---
+++ src/AdditionOfIntWithFloat.java
@@ -0,0 +1,17 @@
+public class AdditionOfIntWithFloat {
+
+    public static void main(String[] args) {
+
+        //int + float → float
+        //float +double ->
+
+        int a = 10;
+        float b =20f;
+
+       int c = (int)(a +b);
+        System.out.println(c);
+       float d = a +b;
+       System.out.println(d);
+
+    }
+}
