581ab1a481644a8801b23c55bf88ecdd8ae63f15
Neha <nehav7.it@gmail.com>
2025-05-16 11:27:07 +0530

    Not Operator Example

---
+++ src/LogicalNotOperator.java
@@ -0,0 +1,15 @@
+public class LogicalNotOperator {
+
+    public static void main(String[] args) {
+
+           boolean lightsOn = false;
+
+           if(!lightsOn)
+           {
+
+              System.out.println("Lights are ON");
+           }
+           else
+               System.out.println("Lights are OFF");
+    }
+}
